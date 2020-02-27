eae5501bdcacec9f34c9ec242be09f1c802c4ec7
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2020-02-27 08:23:43 -0800

    Add files via upload

---
+++ FileDemo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; // Needed for scanner class.
+import java.io.*; // Needed for file handling.
+
+public class FileDemo
+{
+   public static void main(String[] args) throws IOException
+   
+   {
+      File file = new File("test.txt");
+      Scanner inputFile = new Scanner(file); //Creates read file object.
+      PrintWriter outputFile = new PrintWriter("CapsTest.txt"); //Creates write file.
+     
+      
+      while (inputFile.hasNext())
+      {
+         // Read a string from the file.
+         String fileString = inputFile.nextLine();
+         String newString = fileString.toUpperCase();
+         // Print string to new file.
+         outputFile.println(newString);
+       }
+       
+       inputFile.close();
+       outputFile.close();
+ }
+}
+         
+      
