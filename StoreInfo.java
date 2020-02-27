6c47138b27517e517e58cea30c9acaf4159ea0c4
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2020-02-27 11:24:09 -0800

    Add files via upload

---
+++ StoreInfo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner; // Needed for scanner class.
+import java.io.*; // Needed for file handling.
+
+public class StoreInfo
+{
+   public static void main(String[] args) throws IOException
+   {  
+      Scanner kb = new Scanner(System.in); // Creating scanner object.
+      File file = new File("Employee.txt");  // Creates employee file.
+      PrintWriter employeeInfo = new PrintWriter(new FileWriter(file, true)); //Creates write file.
+      System.out.println("How many employees would you like to enter?: " );
+      
+      int employeeNumb =  kb.nextInt(); // Collecting number of employees to enter.
+      kb.nextLine(); //Consume new line
+      
+      for (int i = 1; i <= employeeNumb; i++) // Collect data according to number of employees. 
+      {
+         System.out.println("Enter employees number " + i + ": First name ");
+         String firstName = kb.nextLine();
+         employeeInfo.println(firstName);
+         
+         System.out.println("Enter employee number " + i + ": Last name ");
+         String lastName = kb.nextLine();
+         employeeInfo.println(lastName);
+         
+         System.out.println("Enter employee number " + i + ": Employee ID ");
+         String employeeId = kb.nextLine();
+         employeeInfo.println(employeeId);
+         
+         System.out.println("Enter employee number " + i + ": Years of Experience ");
+         String yearsExp = kb.nextLine();
+         employeeInfo.println(yearsExp);
+      }
+      
+      employeeInfo.close();
+      
+      
+      
+   }
+}
